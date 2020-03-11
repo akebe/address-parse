9354b6a32c75e716e97f814efe478e3c4e9a1b95
akebe <akebe@163.com>
2020-03-11 10:58:22 +0800

    修复地区键名清洗会导致的问题

--- src/parse/parse-area.js
+++ src/parse/parse-area.js
@@ -54,7 +54,10 @@ class ParseArea {
       let area = AREA.area_list[code];
       if (area === '雨花台区') area = '雨花区';
       if (area.length > 2) {
-        ParseArea.AreaShort[code] = AreaKeys.reduce((v, key) => v.replace(key, ''), area);
+        ParseArea.AreaShort[code] = AreaKeys.reduce((v, key) => {
+          if (v.indexOf(key) > 0) v = v.replace(key, '');
+          return v;
+        }, area);
       }
     }
     ParseArea.isInit = true;

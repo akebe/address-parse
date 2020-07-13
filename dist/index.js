9b442bbd836d7a97eb05b8390c3e45cfd2b23e52
akebe <akebe@163.com>
2020-07-13 17:08:42 +0800

    update 1.2.3

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,9 @@ var _parse2 = _interopRequireDefault(_parse);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-window.AddressParse = _parse2.default;
+if (window) {
+  window.AddressParse = _parse2.default;
+}
 
 _parse2.default.Utils = _parse.Utils;
 _parse2.default.AREA = _parse.AREA;
